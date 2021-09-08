e404a283b99875cc4b54866491ebf2cb81bdad97
ErnestoBarraza3pillar <ernesto.barraza@3pillarglobal.com>
2021-09-08 14:09:29 -0700

    docs agregado

---
+++ docs/precache-manifest.00f41405efeb79088a75465d2191bf93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb82fd6c4dc6384c5141251402fcd4f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3c95cbf371c1023afc8",
+    "url": "/static/css/main.32754a9e.chunk.css"
+  },
+  {
+    "revision": "d246ff9353c0306d54eb",
+    "url": "/static/js/2.dc7ccd86.chunk.js"
+  },
+  {
+    "revision": "5f45ba1d7d1825287427a10cffc61124",
+    "url": "/static/js/2.dc7ccd86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3c95cbf371c1023afc8",
+    "url": "/static/js/main.179f068f.chunk.js"
+  },
+  {
+    "revision": "3d4175f9165103e13d56",
+    "url": "/static/js/runtime-main.b8be2bc2.js"
+  }
+]);
